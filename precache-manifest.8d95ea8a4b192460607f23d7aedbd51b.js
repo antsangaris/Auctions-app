eb42e11b1a4164323be885923a3b4db493a7a90a
andreas <tsaggaris-apoel@hotmail.com>
2021-05-24 05:49:18 -0700

    Updates

---
+++ precache-manifest.8d95ea8a4b192460607f23d7aedbd51b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b8db24ad419f06ac78dacaa2ee805a9",
+    "url": "/Auctions-app/index.html"
+  },
+  {
+    "revision": "efc9949dd1d2bd0be68d",
+    "url": "/Auctions-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e024dfd5d5d01881793d",
+    "url": "/Auctions-app/static/js/2.b5b99296.chunk.js"
+  },
+  {
+    "revision": "efc9949dd1d2bd0be68d",
+    "url": "/Auctions-app/static/js/main.bf397f1b.chunk.js"
+  },
+  {
+    "revision": "04e31ba54b408624e9c5",
+    "url": "/Auctions-app/static/js/runtime-main.3c3958fd.js"
+  }
+]);
